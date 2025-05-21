74e03596bccd337d6ff392ac05f796e3d0545086
priyawaw <priyawaw@gmail.com>
2025-05-21 10:27:12 +0530

    locatorsPractice

---
+++ tests/locatorsPractice.spec.ts
@@ -0,0 +1,9 @@
+// CSS selector using ID: #amplify-field-O_JXRsSZ0Fkh, [id='amplify-field-O_JXRsSZ0Fkh'], input#amplify-field-O_JXRsSZ0Fkh
+
+// CSS selector using class: input.amplify-input, .amplify-input, button.amplify-button
+
+// CSS selector using attribute value: [type="submit"], [type="password"], [type="button"]
+
+// CSS selector parent to child: div [type="button"], div [class="amplify-label"], div [class="amplify-heading cursor text-primary pr-4"]
+
+//CSS selector using text:
